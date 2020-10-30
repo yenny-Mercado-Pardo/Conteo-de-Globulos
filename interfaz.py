e8b1d8ab71b431c181ee141bbaf9871d3d561060
bluenet <bluenet@debian.debian>
2020-10-30 15:46:12 -0400

    se ajusto con los nuevos requerimientos

--- interfaz.py
+++ interfaz.py
@@ -6,12 +6,12 @@ import datetime
 X_W = 1200
 Y_H = 600
 RESIZE_IMG = (int(X_W * 0.38-25), int(Y_H*0.95-25))
-
+MEDIDA = "/mm3"
 class Interfaz:
     def __init__(self, master):
         self.master = master
         #titulo de la ventana
-        self.master.title("Taller de redes")
+        self.master.title("Conteo de globulos")
 
         #dimensionando la ventana principal
         self.master.geometry(str(X_W)+ "x" + str(Y_H))
@@ -20,6 +20,7 @@ class Interfaz:
         self.panelControl = Frame(master,width=X_W * 0.24,height=Y_H)
         self.panelControl.pack_propagate(0)
         self.panelControl.pack(side=LEFT)
+        self.panelControl.configure(background='pink')
 
         #creando visualizacion de imagen inicial
         self.visualizacion = Frame(master,width=X_W * 0.38,height=Y_H)
@@ -28,6 +29,7 @@ class Interfaz:
         self.visualizacion.config(cursor="pirate")
         self.visualizacion.config(bd=10)
         self.visualizacion.config(relief="sunken")
+        self.visualizacion.config(background="pink")
         #poniendole un titulo
         self.titulo = Label(self.visualizacion, text="Imagen original")
         self.titulo.pack()
@@ -39,6 +41,7 @@ class Interfaz:
         self.procesado.config(cursor="pirate")
         self.procesado.config(bd=10)
         self.procesado.config(relief="sunken")
+        self.procesado.config(background="pink")
         #poniendo un titulo
         self.tituloProcesado = Label(self.procesado, text="Imagen Procesada")
         self.tituloProcesado.pack()
@@ -55,9 +58,11 @@ class Interfaz:
         # definiendo los controles
         #titulo
         self.tituloControl = Label(self.panelControl, text="Controles",height=2).pack(fill=BOTH)
+
+        self.fontBtn = ('Sans','10','bold')
         #open file
         self.loadFile = Button(self.panelControl,  command=self.open_img, height=2,
-            relief = "groove", compound = CENTER, text="Cargar imagen").pack(fill=BOTH,padx=10, pady=10)
+            relief = "groove", compound = CENTER, text="Cargar imagen",font = self.fontBtn ).pack(fill=BOTH,padx=10, pady=10)
 
         #frame para conectarse a la camara ip
         self.frameCamera = Frame(self.panelControl)
@@ -70,27 +75,34 @@ class Interfaz:
         self.entryLink.insert(0,'http://192.168.0.3:8080/video')
 
         #button de conexion
-        self.connectButton = Button(self.frameCamera,text="CM",command=self.prepareIpCamera)
+        self.connectButton = Button(self.frameCamera,text="C",command=self.prepareIpCamera)
         self.connectButton.grid(row=0,column=1)
 
         #button de captura de imagen
-        self.snaptshotButton = Button(self.frameCamera, text="CA",command=self.snapShotCamera)
+        self.snaptshotButton = Button(self.frameCamera, text="S",command=self.snapShotCamera)
         self.snaptshotButton.grid(row=0, column=2)
+
+        # para el video
+        self.stopCamera = Button(self.frameCamera, text="[]", command=self.stopStreamVideo)
+        self.stopCamera.grid(row=0, column=3)
 
 
         #aplicando el analisis
         self.applyProceso = Button(self.panelControl, command=self.procesarOpencv, height=2,
-                           relief="groove", compound=CENTER, text="Procesar").pack(fill=BOTH, padx=10,pady=10)
+                           relief="groove", compound=CENTER, text="Procesar",font = self.fontBtn).pack(fill=BOTH, padx=10,pady=10)
 
         # aplicando el analisis
-        self.applyStepProceso = Button(self.panelControl, command=self.callStepProcess, height=2,
-                        relief="groove", compound=CENTER, text="Paso por paso").pack(fill=BOTH, padx=10)
+        #self.applyStepProceso = Button(self.panelControl, command=self.callStepProcess, height=2,
+        #                relief="groove", compound=CENTER, text="Paso por paso").pack(fill=BOTH, padx=10)
+
+        self.clearProcess = Button(self.panelControl, command=self.limpiarVisualizador, height=2,
+                        relief="groove", compound=CENTER, text="Limpiar",font = self.fontBtn).pack(fill=BOTH, padx=10)
 
         self.valor = IntVar()
         self.valor.set(1)
-        self.radioButton1 = Radiobutton(self.panelControl, text="total(blanco) = XXX * 20 * 2.5 (mm3)", variable=self.valor, value=1)
+        self.radioButton1 = Radiobutton(self.panelControl, text="total(blanco) = XXX * 20 * 2.5 "+MEDIDA, variable=self.valor, value=1)
         self.radioButton1.pack(fill=BOTH, padx=10)
-        self.radioButton2 = Radiobutton(self.panelControl, text="total(rojo) = XXX * 200 * 10 * 5 (mmm3)", variable=self.valor, value=2)
+        self.radioButton2 = Radiobutton(self.panelControl, text="total(rojo) = XXX * 200 * 10 * 5 "+MEDIDA, variable=self.valor, value=2)
         self.radioButton2.pack(fill=BOTH, padx=10)
 
         #scroll a modo de consola
@@ -104,10 +116,15 @@ class Interfaz:
         self.streamVideo = None
 
     def calcularRojos(self,cantidad):
-        self.printLn("C. eritrocitos " + str(cantidad * 200 * 10 * 5 ) + " mm3")
+        self.printLn("C. eritrocitos " + str(cantidad * 200 * 10 * 5 ) + MEDIDA)
 
     def calcularBlancos(self, cantidad):
-        self.printLn("C. leucocitos " + str(cantidad * 20 * 2.5) + " mm3")
+        self.printLn("C. leucocitos " + str(cantidad * 20 * 2.5) + MEDIDA)
+
+    def stopStreamVideo(self):
+        self.captureStrem = False
+        self.streamVideo = None
+        self.clearOriginal()
 
     def prepareIpCamera(self):
         self.connectCamera()
@@ -118,6 +135,7 @@ class Interfaz:
         try:
             self.printLn("Connect: " + link )
             self.streamVideo = cv2.VideoCapture(link)
+            self.clearProcesado()
         except:
             self.printLn("Verifique la direccion, no se ha podido conectar")
 
@@ -169,9 +187,22 @@ class Interfaz:
         else:
             self.calcularBlancos(cantidad)
 
+    def limpiarVisualizador(self):
+        self.clearOriginal()
+        self.clearProcesado()
+        self.pathOriginal= None
+
+    def clearOriginal(self):
+        self.panel.config(image=None)
+        self.panel.image = None
+
+    def clearProcesado(self):
+        self.panelProcesado.config(image=None)
+        self.panelProcesado.image = None
+
     def procesarOpencv(self):
         if self.pathOriginal is None:
-            self.printLn("Sebe seleccionar la imagen")
+            self.printLn("Debe seleccionar una imagen")
             return
         procesarImg = ProcesamientoImagen(self.pathOriginal)
         array,region = procesarImg.procesar()
@@ -199,6 +230,7 @@ class Interfaz:
         img = Image.open(x)
         img = img.resize(RESIZE_IMG, Image.BICUBIC)
         self.showFrameImage(img)
+        self.clearProcesado()
 
     def openfn(self):
         filename = filedialog.askopenfilename(title='open')
